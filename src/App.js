16a9c6af62b29f1713837bdcb7b23feddeea074c
Denis Agafonov <d.a6af0n0v@gmail.com>
2023-10-05 20:23:00 +0200

    Added: navbar, measurepage with a button

--- src/App.js
+++ src/App.js
@@ -1,57 +1,81 @@
+import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
 require('bootstrap');
 
-function Accordion() {
+function Menu() {
   return (
-    <div className="accordion" id="accordionExample">
-      <div className="accordion-item">
-        <h2 className="accordion-header">
-          <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Accordion Item #1
-          </button>
-        </h2>
-        <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-          <div className="accordion-body">
-            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-          </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">FaradaIC</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Measure</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">Sensors</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Measurements</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="login">
+            <button className="btn btn-outline-success" type="submit">Login</button>
+          </form>
         </div>
       </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header">
-          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Accordion Item #2
-          </button>
-        </h2>
-        <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-          <div className="accordion-body">
-            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-          </div>
+    </nav>
+  );
+}
+
+function MeasurePage() {
+  const [msg, setMsg] = useState({ text: "", visible: false, type: "alert alert-danger" });
+  async function selectPortClicked() {
+    console.log("Select port");
+    try {
+      let port = await navigator.serial.requestPort();
+      let info = await port.getInfo();
+      console.log(info);
+    } catch (error) {
+      setMsg({ text: error.toString(), visible: true, type: "alert alert-warning" });
+      console.log(error);
+    }
+  }
+  return (
+    <>
+      <h5>FaradaIC Sensor Dashboard</h5>
+      {
+        msg.visible &&
+        <div className={msg.type} role="alert" >
+          {msg.text}
+        </div>
+      }
+      <div className='row'>
+        <div className='col-2'>
+          <button className='btn btn-primary'
+            onClick={selectPortClicked}
+          >Select Port</button>
         </div>
       </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header">
-          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Accordion Item #3
-          </button>
-        </h2>
-        <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-          <div className="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
 function App() {
   return (
-    <div classNameName="App">
-      <h1>FaradaIC Sensor Dashboard</h1>
-      <Accordion />
-
-    </div>
+    <div className="App container">
+      <div className='row'>
+        <Menu />
+      </div>
+      <div className='row'>
+        <MeasurePage />
+      </div>
+    </div >
   );
 }
 
